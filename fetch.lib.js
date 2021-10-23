6b6aa8c6c5c6d7768e702dc971bad23948f74654
Firas Kudsy <firaskudsy@gmail.com>
2021-10-23 14:27:21 -0400

    Update fetch.lib.js

--- fetch.lib.js
+++ fetch.lib.js
@@ -5,5 +5,5 @@ const fetchJSON = async (url) => await fetch(url)
   .then(response => response.json())
 
 
-const response = fetchJSON('https://jsonplaceholder.typicode.com/todos');
+const response = await fetchJSON('https://jsonplaceholder.typicode.com/todos');
 console.log('DEBUG: ~ file: fetch.lib.js ~ line 9 ~ response', response);
