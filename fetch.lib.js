7df061578718df6207e8a9a10cfcb691feb39a8d
Firas Kudsy <firaskudsy@gmail.com>
2021-10-23 14:25:24 -0400

    Update fetch.lib.js

--- fetch.lib.js
+++ fetch.lib.js
@@ -1,3 +1,5 @@
 // public Rest-API for testing
 
-const fetchJSON = url => 
+const fetchJSON = url => fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
